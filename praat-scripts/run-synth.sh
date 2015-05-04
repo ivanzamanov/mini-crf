e5b10423033a2815b7b26cdd728658a77bc50433
Ivan Zamanov <ivo.zamanov@gmail.com>
2015-05-04 20:21:01 +0300

    Slight script improvement

--- praat-scripts/run-synth.sh
+++ praat-scripts/run-synth.sh
@@ -6,13 +6,19 @@ BASE=$(readlink -f $(dirname $0))
 TEMP_F=$(mktemp)
 SENT="$(echo "$1" | tr ' ' '_')"
 
+SYNTH_DB=~/SpeechSynthesis/db-synth-preemph.bin
+TEST_DB=~/SpeechSynthesis/db-test-preemph.bin
+
 pushd $BASE/../src
+
 SYNTH_TMP=$(mktemp)
-./main --mode query --synth-database db-test.bin --sentence --input $SENT > $SYNTH_TMP
+./main --mode query --synth-database $TEST_DB --sentence --input $SENT > $SYNTH_TMP
 echo "Synth temp file: $SYNTH_TMP"
 echo "Concat temp file: $TEMP_F"
-./main --mode synth --synth-database db-synth.bin --input - --textgrid $BASE/concat.TextGrid > "$TEMP_F" < $SYNTH_TMP
+./main-opt --mode synth --synth-database $SYNTH_DB --input - --textgrid $BASE/concat.TextGrid > "$TEMP_F" < $SYNTH_TMP
 popd
 
+set +x
 fix_synth_output "$TEMP_F"
+set -x
 praat "$(os_path $BASE/concat.praat)" "$(os_path $TEMP_F)" concat-output.wav
