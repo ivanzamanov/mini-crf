8c346a1f107515e354e22bfdfe6f16d63363ed64
Ivan Zamanov <ivo.zamanov@gmail.com>
2015-05-10 15:47:14 +0300

    Parallelize feature extraction with xargs

--- praat-scripts/features-extract.sh
+++ praat-scripts/features-extract.sh
@@ -21,6 +21,7 @@ FILES_LIST=$OUTPUT_PATH/files-list
 
 WAVS=0
 GRIDS=0
+set +x
 for WAV in $(find $CORPUS_PATH | grep wav$)
 do
     WAVS=$(expr $WAVS + 1)
@@ -28,13 +29,13 @@ do
     GRID_NAME=$(echo $BASE | tr . _)
     GRID=$(dirname $WAV)/$GRID_NAME.TextGrid
     if [ -f $GRID ]; then
-        echo "Extracting from $WAV"
+        #echo "Extracting from $WAV"
 				OUTPUT=$(os_path $OUTPUT_PATH/"$GRID_NAME.Features")
 				echo "$OUTPUT $WAV" >> $FILES_LIST
-        $EXTRACTOR_SCRIPT "$WAV" "$GRID" "$OUTPUT" >> commands
+        echo "$WAV" "$GRID" "$OUTPUT"
         GRIDS=`expr $GRIDS + 1`
     fi
-done
+done | xargs -P 4 -n 3 -t $EXTRACTOR_SCRIPT
 
 echo "WAVS: $WAVS"
 echo "Grids: $GRIDS"
