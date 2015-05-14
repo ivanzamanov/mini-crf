bb7a1e6cc6d4f1a9a37e2e0743768f4a27d4fa4d
Ivan Zamanov <ivo.zamanov@gmail.com>
2015-05-14 17:39:05 +0300

    Rename...

---
+++ scripts/functions.sh
@@ -0,0 +1,15 @@
+
+function os_path {
+  # cygpath -w "$1"
+  echo "$1"
+}
+
+function fix_synth_output {
+  # sed -i 's/$/\r/g' $1
+  # for file in $(grep -o '/.*wav' $1)
+  # do
+  #   REPL="$(os_path "$file" | sed 's/\\/\\\\/g')"
+  #   sed -i s\|"$file"\|"$REPL"\|g $1
+  # done
+  echo
+}
