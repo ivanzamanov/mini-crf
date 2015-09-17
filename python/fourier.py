1ea33a910221cc095ce9839ffde67e8bbfed1079
Ivan Zamanov <ivo.zamanov@gmail.com>
2015-09-18 01:10:24 +0300

    Want to see what effects playing with FD values has

--- python/fourier.py
+++ python/fourier.py
@@ -1,18 +1,18 @@
 from math import *
 from opster import command
 
-from functools32 import lru_cache
+from functools import lru_cache
 import matplotlib.pyplot as plt
 
 def frange(start, end, step):
     start += 0.0
     step += 0.0
     end += 0.0
-    return [start + x * step for x in xrange(int((end - start) / step))]
+    return [start + x * step for x in range(0, int((end - start) / step) + 1)]
 
 def myF(t):
     #return 5 + 2 * cos(2 * pi * t - pi / 2) + 3 * cos(4 * pi * t)
-    return cos(t)
+    return sin(t)
 
 def FT(func, T, step=1.0):
     (sT, eT) = T
@@ -38,25 +38,29 @@ def rFT(func, F, step=1.0):
             real = real * cos(2 * pi * time * f)
             img = img * sin (2 * pi * time * f)
             result = result + real - img
-        return result
+        return 1 / (2 * pi) * result
     return result
 
 @command()
 def main():
     period = 2 * pi
-    freqStep = 1 / period
-    freqRange = (- 5 / period, 5 / period)
     timeRange = (-period / 2, period / 2)
-    timeStep = 0.001
+    timeStep = 0.01
+
+    freqStep = 1 / period
+    maxFreqComponents = 4
+    freqRange = (- (maxFreqComponents / 2) / period, (maxFreqComponents / 2) / period)
 
     ft = FT(myF, timeRange, timeStep)
     rft = rFT(ft, freqRange, freqStep)
 
+    tdVal = [myF(t) for t in frange(timeRange[0], timeRange[1], timeStep)]
+    fdVal = [ft(f) for f in frange(freqRange[0], freqRange[1], freqStep)]
+
     plot = plt.subplot(311)
     plot.set_title("Fourier Domain")
     x = [x for x in frange(freqRange[0], freqRange[1], freqStep)]
-    y = [ft(t) for t in x]
-
+    y = fdVal
     plot.plot(x, [a for (a,b) in y], 'b')
     plot.plot(x, [b for (a,b) in y], 'r')
 
@@ -69,7 +73,7 @@ def main():
 
     plot = plt.subplot(313)
     plot.set_title("Time Domain Original")
-    plot.plot(x, [myF(t) for t in x], 'b')
+    plot.plot(x, tdVal, 'b')
 
     plt.show()
 
