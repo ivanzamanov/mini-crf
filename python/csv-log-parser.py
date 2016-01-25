d1f65c1c6e95119b5168f17482c878535bc9986f
Ivan Zamanov <ivo.zamanov@gmail.com>
2016-01-26 01:02:28 +0200

    Log parser and plotting done

--- python/csv-log-parser.py
+++ python/csv-log-parser.py
@@ -17,15 +17,32 @@ def getValues(line):  return line.split(' ')
 def main(logFilePath):
     lines = readLines(logFilePath)
     headers = getValues(next(lines))
-    allValues = map( lambda x: map(float, x), map(getValues, lines))
+    maxFeatures = len(headers) - 1
 
     print "Plotting"
-    targetValues = [ values[len(values) - 1] for values in allValues ]
-    plt.plot(targetValues)
-    plt.axhline( min(targetValues), color='r')
-    for i, (prev, n) in enumerate(zip(allValues, allValues[1:])):
-        if i % 1000 == 0:
-            plt.axvline(i, color='r')
+    #plt.plot(targetValues)
+
+    nextFeatureIndex = 0
+    nextX = 0
+    currentPlot = None
+    for line in lines:
+        if line.startswith('#'):
+            if currentPlot:
+                currentPlot.plot(x, y)
+                #break
+            x = []
+            y = []
+            feature = line[1:]
+            currentPlot = plt.subplot(maxFeatures, 1, nextFeatureIndex + 1)
+            currentPlot.set_title( feature )
+            nextX = 0
+            nextFeatureIndex = (nextFeatureIndex + 1) % maxFeatures
+        else:
+            value = float(getValues(line)[-1])
+            #print value, nextX
+            nextX += 1
+            x.append(nextX)
+            y.append(value)
     plt.show();
 
 main.command()
